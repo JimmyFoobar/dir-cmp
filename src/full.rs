a86eb0f09f1bdbc169243246d3b258d2693db315
JImmyFoobar <jimmy.foobar@gmail.com>
2023-01-29 20:02:22 +0100

    adding test cases + rename DirCmpEntry

--- src/full.rs
+++ src/full.rs
@@ -7,7 +7,7 @@ use crate::{
 };
 
 #[derive(PartialEq, Eq, Debug)]
-pub enum DirCompEntry {
+pub enum DirCmpEntry {
     Both(PathBuf, PathBuf, FileCompResult),
     Left(PathBuf),
     Right(PathBuf),
@@ -19,10 +19,10 @@ fn compare_dirs_inner(
     left_base: &str,
     right_base: &str,
     options: &Options,
-) -> io::Result<Vec<DirCompEntry>> {
+) -> io::Result<Vec<DirCmpEntry>> {
     trace!("comparing 2 dirs");
 
-    let mut results: Vec<DirCompEntry> = Vec::new();
+    let mut results: Vec<DirCmpEntry> = Vec::new();
     for dir_entry in zip_dir_entries(
         &left_path.to_path_buf(),
         &right_path.to_path_buf(),
@@ -36,7 +36,7 @@ fn compare_dirs_inner(
                 if left_entry.is_file() && right_entry.is_file() {
                     let comp_result = compare_two_files(&left_entry, &right_entry)?;
                     if FileCompResult::Equal != comp_result || !options.ignore_equal {
-                        results.push(DirCompEntry::Both(
+                        results.push(DirCmpEntry::Both(
                             left_entry.to_owned(),
                             right_entry.to_owned(),
                             comp_result,
@@ -56,19 +56,19 @@ fn compare_dirs_inner(
                     results.extend(subtree_results);
                 }
 
-                //ignore symlinks and mismatches s
+                //ignore symlinks and mismatches
             }
             EitherOrBoth::Left(left_entry) => {
                 if !options.ignore_left_only {
                     if left_entry.is_dir() {
                         let entry_list = list_files(&left_entry);
                         for file_path in entry_list {
-                            results.push(DirCompEntry::Left(file_path));
+                            results.push(DirCmpEntry::Left(file_path));
                         }
                         continue;
                     }
                     if left_entry.is_file() {
-                        results.push(DirCompEntry::Left(left_entry));
+                        results.push(DirCmpEntry::Left(left_entry));
                         continue;
                     }
                     if left_entry.is_symlink() {
@@ -82,12 +82,12 @@ fn compare_dirs_inner(
                     if right_entry.is_dir() {
                         let entry_list = list_files(&right_entry);
                         for file_path in entry_list {
-                            results.push(DirCompEntry::Right(file_path));
+                            results.push(DirCmpEntry::Right(file_path));
                         }
                         continue;
                     }
                     if right_entry.is_file() {
-                        results.push(DirCompEntry::Right(right_entry));
+                        results.push(DirCmpEntry::Right(right_entry));
                         continue;
                     }
                     if right_entry.is_symlink() {
@@ -101,11 +101,215 @@ fn compare_dirs_inner(
     Ok(results)
 }
 
+#[cfg(test)]
+mod tests_compare_dirs_inner {
+    use super::*;
+    use std::fs;
+
+    fn init_logger() {
+        let _ = env_logger::builder().is_test(true).try_init();
+    }
+
+    #[test]
+    fn no_restictions() {
+        init_logger();
+        //prepare left dir
+        let left_dir = tempfile::Builder::new().tempdir().unwrap();
+        let file_left_both_equal = left_dir.path().join("both_equal.txt");
+        fs::write(file_left_both_equal.as_path(), b"same same").unwrap();
+        let file_left_both_diff = left_dir.path().join("both_diff.txt");
+        fs::write(file_left_both_diff.as_path(), b"differnt").unwrap();
+        let file_left_only = left_dir.path().join("left_only.txt");
+        fs::write(file_left_only.as_path(), b"Lefty left").unwrap();
+
+        //prepare right dir
+        let right_dir = tempfile::Builder::new().tempdir().unwrap();
+        let file_right_both_equal = right_dir.path().join("both_equal.txt");
+        fs::write(file_right_both_equal.as_path(), b"same same").unwrap();
+        let file_right_both_diff = right_dir.path().join("both_diff.txt");
+        fs::write(file_right_both_diff.as_path(), b"more different").unwrap();
+        let file_right_only = right_dir.path().join("right_only.txt");
+        fs::write(file_right_only.as_path(), b"Righty right").unwrap();
+
+        //create options without any restrictions
+        let diff_options = Options {
+            ignore_left_only: false,
+            ignore_right_only: false,
+            filter: None,
+            ignore_equal: false,
+        };
+
+        let expected: Vec<DirCmpEntry> = vec![
+            DirCmpEntry::Left(file_left_only.as_path().to_path_buf()),
+            DirCmpEntry::Both(
+                file_left_both_diff.as_path().to_path_buf(),
+                file_right_both_diff.as_path().to_path_buf(),
+                FileCompResult::Different,
+            ),
+            DirCmpEntry::Both(
+                file_left_both_equal.as_path().to_path_buf(),
+                file_right_both_equal.as_path().to_path_buf(),
+                FileCompResult::Equal,
+            ),
+            DirCmpEntry::Right(file_right_only.as_path().to_path_buf()),
+        ];
+
+        //compare
+        let result = compare_dirs(left_dir.path(), right_dir.path(), diff_options).unwrap();
+
+        assert_eq!(result, expected);
+    }
+    #[test]
+    fn ignore_equal() {
+        init_logger();
+        //prepare left dir
+        let left_dir = tempfile::Builder::new().tempdir().unwrap();
+        let file_left_both_equal = left_dir.path().join("both_equal.txt");
+        fs::write(file_left_both_equal.as_path(), b"same same").unwrap();
+        let file_left_both_diff = left_dir.path().join("both_diff.txt");
+        fs::write(file_left_both_diff.as_path(), b"differnt").unwrap();
+        let file_left_only = left_dir.path().join("left_only.txt");
+        fs::write(file_left_only.as_path(), b"Lefty left").unwrap();
+
+        //prepare right dir
+        let right_dir = tempfile::Builder::new().tempdir().unwrap();
+        let file_right_both_equal = right_dir.path().join("both_equal.txt");
+        fs::write(file_right_both_equal.as_path(), b"same same").unwrap();
+        let file_right_both_diff = right_dir.path().join("both_diff.txt");
+        fs::write(file_right_both_diff.as_path(), b"more different").unwrap();
+        let file_right_only = right_dir.path().join("right_only.txt");
+        fs::write(file_right_only.as_path(), b"Righty right").unwrap();
+
+        //create options without any restrictions
+        let diff_options = Options {
+            ignore_equal: true,
+            ignore_left_only: false,
+            ignore_right_only: false,
+            filter: None,
+        };
+
+        let expected: Vec<DirCmpEntry> = vec![
+            DirCmpEntry::Left(file_left_only.as_path().to_path_buf()),
+            DirCmpEntry::Both(
+                file_left_both_diff.as_path().to_path_buf(),
+                file_right_both_diff.as_path().to_path_buf(),
+                FileCompResult::Different,
+            ),
+            DirCmpEntry::Right(file_right_only.as_path().to_path_buf()),
+        ];
+
+        //compare
+        let result = compare_dirs(left_dir.path(), right_dir.path(), diff_options).unwrap();
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn ignore_left_only() {
+        init_logger();
+        //prepare left dir
+        let left_dir = tempfile::Builder::new().tempdir().unwrap();
+        let file_left_both_equal = left_dir.path().join("both_equal.txt");
+        fs::write(file_left_both_equal.as_path(), b"same same").unwrap();
+        let file_left_both_diff = left_dir.path().join("both_diff.txt");
+        fs::write(file_left_both_diff.as_path(), b"differnt").unwrap();
+        let file_left_only = left_dir.path().join("left_only.txt");
+        fs::write(file_left_only.as_path(), b"Lefty left").unwrap();
+
+        //prepare right dir
+        let right_dir = tempfile::Builder::new().tempdir().unwrap();
+        let file_right_both_equal = right_dir.path().join("both_equal.txt");
+        fs::write(file_right_both_equal.as_path(), b"same same").unwrap();
+        let file_right_both_diff = right_dir.path().join("both_diff.txt");
+        fs::write(file_right_both_diff.as_path(), b"more different").unwrap();
+        let file_right_only = right_dir.path().join("right_only.txt");
+        fs::write(file_right_only.as_path(), b"Righty right").unwrap();
+
+        //create options without any restrictions
+        let diff_options = Options {
+            ignore_equal: false,
+            ignore_left_only: true,
+            ignore_right_only: false,
+            filter: None,
+        };
+
+        let expected: Vec<DirCmpEntry> = vec![
+            DirCmpEntry::Both(
+                file_left_both_diff.as_path().to_path_buf(),
+                file_right_both_diff.as_path().to_path_buf(),
+                FileCompResult::Different,
+            ),
+            DirCmpEntry::Both(
+                file_left_both_equal.as_path().to_path_buf(),
+                file_right_both_equal.as_path().to_path_buf(),
+                FileCompResult::Equal,
+            ),
+            DirCmpEntry::Right(file_right_only.as_path().to_path_buf()),
+        ];
+
+        //compare
+        let result = compare_dirs(left_dir.path(), right_dir.path(), diff_options).unwrap();
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn ignore_right_only() {
+        init_logger();
+        //prepare left dir
+        let left_dir = tempfile::Builder::new().tempdir().unwrap();
+        let file_left_both_equal = left_dir.path().join("both_equal.txt");
+        fs::write(file_left_both_equal.as_path(), b"same same").unwrap();
+        let file_left_both_diff = left_dir.path().join("both_diff.txt");
+        fs::write(file_left_both_diff.as_path(), b"differnt").unwrap();
+        let file_left_only = left_dir.path().join("left_only.txt");
+        fs::write(file_left_only.as_path(), b"Lefty left").unwrap();
+
+        //prepare right dir
+        let right_dir = tempfile::Builder::new().tempdir().unwrap();
+        let file_right_both_equal = right_dir.path().join("both_equal.txt");
+        fs::write(file_right_both_equal.as_path(), b"same same").unwrap();
+        let file_right_both_diff = right_dir.path().join("both_diff.txt");
+        fs::write(file_right_both_diff.as_path(), b"more different").unwrap();
+        let file_right_only = right_dir.path().join("right_only.txt");
+        fs::write(file_right_only.as_path(), b"Righty right").unwrap();
+
+        //create options without any restrictions
+        let diff_options = Options {
+            ignore_equal: false,
+            ignore_left_only: false,
+            ignore_right_only: true,
+            filter: None,
+            
+        };
+
+        let expected: Vec<DirCmpEntry> = vec![
+            DirCmpEntry::Left(file_left_only.as_path().to_path_buf()),
+            DirCmpEntry::Both(
+                file_left_both_diff.as_path().to_path_buf(),
+                file_right_both_diff.as_path().to_path_buf(),
+                FileCompResult::Different,
+            ),
+            DirCmpEntry::Both(
+                file_left_both_equal.as_path().to_path_buf(),
+                file_right_both_equal.as_path().to_path_buf(),
+                FileCompResult::Equal,
+            ),
+        ];
+
+        //compare
+        let result = compare_dirs(left_dir.path(), right_dir.path(), diff_options).unwrap();
+
+        assert_eq!(result, expected);
+    }
+}
+
+
 pub fn compare_dirs(
     left_path: &Path,
     right_path: &Path,
     options: Options,
-) -> io::Result<Vec<DirCompEntry>> {
+) -> io::Result<Vec<DirCmpEntry>> {
     debug!(
         "starting to compare for {:?} vs {:?}",
         left_path, right_path
